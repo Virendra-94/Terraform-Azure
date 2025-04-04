d8f6a8564bc9d85af94f9432e75bd22bba2a44b5
Virendra-94 <virendrakumarofficial94@gmail.com>
2025-04-04 14:21:52 +0530

    Some files deleted

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-rg-vir"
-    storage_account_name = "terraformvirenstorage"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
